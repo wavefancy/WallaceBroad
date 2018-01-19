f0bc634187beaa5d7e80094dcf3b4894dc5bec5f
wallace-mac <wavefancy@gmail.com>
2018-01-18 21:21:51 -0500

    merged

--- python/vcf/GenotypeConcordance/bash.sh
+++ python/vcf/GenotypeConcordance/bash.sh
@@ -1,4 +1,4 @@
-<<<<<<< HEADcat test.txt | python3 GCCategorySummay.py
+cat test.txt | python3 GCCategorySummay.py
 #Label   correct_00      correct_01      correct_11      error_00        error_01        error_11        errorRate_00   errorRate_01    errorRate_1     errorRate_Total
 #T       76      2       0       0       2       0       0.0000e+00      5.0000e-01      NA      2.5000e-02
 #A       114     2       0       1       0       1       8.6957e-03      0.0000e+00      1.0000e+00      1.6949e-02
