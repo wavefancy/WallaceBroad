fdf05fe6c0c0c78fcfa1ee13083dad882e7132ef
Wallace Wang <wavefancy@gmail.com>
2018-05-16 11:20:03 -0400

    version 1.0

---
+++ python/stats/Categorical2Dummy/Categorical2Dummy.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+
+    Convert categorical data to dummy variable.
+
+    @Author: wavefancy@gmail.com
+
+    Usage:
+        Categorical2Dummy.py -n name
+        Categorical2Dummy.py -h | --help | -v | --version | -f | --format
+
+    Notes:
+        1. Read from stdin and output to stdout.
+        2. Output results from stdout.
+        3. Treat the first line as header.
+
+    Options:
+        -n name       Column name for the column to be converted.
+        -h --help     Show this screen.
+        -v --version  Show version.
+        -f --format   Show input/output file format example.
+
+"""
+#   Ref:
+#        1. https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
+#
+import sys
+from docopt import docopt
+from signal import signal, SIGPIPE, SIG_DFL
+
+def ShowFormat():
+    '''Input File format example:'''
+    print('''
+#input example
+------------------------
+X1      C
+1       C1
+2       C2
+3       C1
+4       C3
+5       C2
+6       C2
+
+#cat test.txt |  cat test.txt | python3 Categorical2Dummy.py -n C
+------------------------
+X1      C       C_C1    C_C2    C_C3
+1       C1      1       0       0
+2       C2      0       1       0
+3       C1      1       0       0
+4       C3      0       0       1
+5       C2      0       1       0
+6       C2      0       1       0
+    ''');
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='1.0')
+    # print(args)
+    if(args['--format']):
+        ShowFormat()
+        sys.exit(-1)
+
+    CNAME = args['-n']
+    import pandas as pd
+
+    data = pd.read_table(sys.stdin,header=0,delim_whitespace=True)
+    if CNAME not in data.columns:
+        sys.stderr.write('ERROR: can not find column name in input header, key: %s\n'%(CNAME))
+        sys.exit(-1)
+
+    dummy = pd.get_dummies(data[(CNAME)],prefix=CNAME)
+    # cols = [x for x in data.columns if x != CNAME]
+    results = pd.concat([data,dummy], axis=1)
+    results.to_csv(sys.stdout, sep='\t',index=False)
+
+sys.stdout.flush()
+sys.stdout.close()
+sys.stderr.flush()
+sys.stderr.close()
