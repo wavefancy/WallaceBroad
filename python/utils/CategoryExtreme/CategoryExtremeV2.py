9ed6dda727a8e58f1b8caebde3811cf570a6f985
Wallace Wang <wavefancy@gmail.com>
2018-06-13 13:55:29 -0400

    add function for sorting string, and in preset order

---
+++ python/utils/CategoryExtreme/CategoryExtremeV2.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""
+
+    Select the extreme value for each category.
+
+    @Author: wavefancy@gmail.com
+
+    Usage:
+        CategoryExtremeV2.py -k ints -v int [-o txt | --txt] (--max | --min) [-a]
+        CategoryExtremeV2.py -h | --help | --version | -f | --format
+
+    Notes:
+        1. Read data from stdin (file name line by line), and output results to stdout.
+
+    Options:
+        -k ints     Column index for category, 1 based. eg. 1|1,3.
+        -v int      Column index for value, 1 based. eg. 1.
+        -o txt      Order for ranking category data, small to large. eg. ONE,TWO,THREE.
+        -a          Select all entries with extreme value, default random pick one.
+        --txt       Sort by value text string naturally, do not convert to float.
+        --max       Select the maxmium values for each category.
+        --min       Select the minimum values for each category.
+        --version  Show version.
+        -f --format   Show input/output file format example.
+"""
+import sys
+from docopt import docopt
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE, SIG_DFL)
+import os.path as path
+
+def ShowFormat():
+    '''Input File format example:'''
+    print('''
+
+    ''');
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='1.0')
+    # print(args)
+    # sys.exit(-1)
+
+    if(args['--format']):
+        ShowFormat()
+        sys.exit(-1)
+
+    CATEGORYS   = [int(x)-1 for x in args['-k'].split(',')]
+    VALUE       = int(args['-v'])-1
+    MAX_VAL     = True if args['--max'] else False
+    ALL_EXTREME = True if args['-a'] else False
+    ORDERS      = args['-o'].split(',') if args['-o'] else []
+    ORDERS_MAP  = {k:i for (i,k) in enumerate(ORDERS)}
+    TEXT_SORT   = True if args['--txt'] else False
+
+    def processOne(entries):
+        '''find out the extreme value and output'''
+        ee = ''
+        vv = []
+        if TEXT_SORT:
+            if MAX_VAL:
+                entries.sort(key=lambda x: x[1][VALUE], reverse=True)
+            else:
+                entries.sort(key=lambda x: x[1][VALUE])
+
+        elif ORDERS:
+            if MAX_VAL:
+                entries.sort(key=lambda x: ORDERS_MAP[x[1][VALUE]], reverse=True)
+            else:
+                entries.sort(key=lambda x: ORDERS_MAP[x[1][VALUE]])
+
+        else:
+            if MAX_VAL:
+                entries.sort(key=lambda x: float(x[1][VALUE]), reverse=True)
+            else:
+                entries.sort(key=lambda x: float(x[1][VALUE]))
+
+        #output results after sort.
+        if ALL_EXTREME:
+            for _,v in entries:
+                if v[VALUE] == entries[0][1][VALUE]:
+                    sys.stdout.write('\t'.join(v))
+                    sys.stdout.write('\n')
+                else:
+                    break
+        else:
+            sys.stdout.write('\t'.join(entries[0][1]))
+            sys.stdout.write('\n')
+
+    #read from stdin.
+    c_temp = [] #[(entryName,entry1), (entryName,entry2)... ] #entries for a category.
+    for line in sys.stdin:
+        line = line.strip()
+        if line:
+            ss = line.split()
+            name = ','.join([ss[x] for x in CATEGORYS])
+            if c_temp:
+                if c_temp[-1][0] == name:
+                    c_temp.append((name,ss))
+                else:
+                    processOne(c_temp)
+                    c_temp = []
+                    c_temp.append((name,ss))
+            else:
+                c_temp.append((name,ss))
+
+    #deal with the last category.
+    processOne(c_temp)
+
+sys.stdout.flush()
+sys.stdout.close()
+sys.stderr.flush()
+sys.stderr.close()
--- python/utils/CategoryExtreme/bash.sh
+++ python/utils/CategoryExtreme/bash.sh
@@ -1,8 +1,17 @@
 
-cat test.txt | python CategoryExtreme.py 1,2 3 i
+cat test.txt | tail -n +1 | python3 CategoryExtremeV2.py -k2 -v 3 --txt --min
+#1       Title   t
+#2       ss      1
+#4       s1      0
+#5       s2      4              
 
-#Title   t
-#ss      1
-#s1      0
-#s2      4
+cat test.txt | tail -n +2 | python3 CategoryExtremeV2.py -k2 -v 3 --max -a
+#3       ss      3
+#4       ss      3
+#4       s1      1
+#5       s2      4
 
+cat test.txt | tail -n +2 | python3 CategoryExtremeV2.py -k2 -v 3 -o 4,3,2,1,0 --max
+#2       ss      1
+#4       s1      0
+#5       s2      4
