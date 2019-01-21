9b1e842a959369083553492be1ab331c083344bb
Wallace Wang <wavefancy@gmail.com>
2019-01-21 00:53:26 -0500

    add function to output imputation genotype count

--- python/vcf/VCFAlleleCountsFre/VCFAlleleCountsFre.py
+++ python/vcf/VCFAlleleCountsFre/VCFAlleleCountsFre.py
@@ -9,7 +9,7 @@
     @Author: wavefancy@gmail.com
 
     Usage:
-        VCFAlleleCountsFre.py
+        VCFAlleleCountsFre.py [-c txt]
         VCFAlleleCountsFre.py -h | --help | -v | --version | -f | --format
 
     Notes:
@@ -18,12 +18,16 @@
         3. TotalCount: total non-missing ref+alt count.
 
     Options:
+        -c txt          Specify statistics to calculate,
+                          default: ALT_FRE: calculate alt allele frequency.
+                           option: GP_GENO: genotype count based on imputation GP.
         -h --help       Show this screen.
         -v --version    Show version.
         -f --format     Show format example.
 """
 import sys
 from docopt import docopt
+import numpy as np
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE, SIG_DFL)
 
@@ -52,6 +56,17 @@ if __name__ == '__main__':
     vcfMetaCols=9       #number of colummns for vcf meta information.
     tags = ['GT']
     OUT_FORMAT = 'ALT_FRE'
+    if args['-c'] and args['-c'] not in ['ALT_FRE', 'GP_GENO']:
+        sys.stderr.write('PLEASE set a proper value for "-c"!\n')
+        sys.exit(-1)
+
+    if args['-c'] == 'GP_GENO':
+        OUT_FORMAT = 'GP_GENO'
+        tags = ['GP']
+        sys.stdout.write('#CHROM\tPOS\tREF\tALT\tCOUNT_0/0\tCOUNT_0/1\tCOUNT_1/1\n')
+
+    if OUT_FORMAT == 'ALT_FRE':
+        sys.stdout.write('#CHROM\tPOS\tREF\tALT\tTotalCount\tAltCount\tAltFre\n')
 
     def getGeno(geno):
         '''get genotype info.'''
@@ -104,16 +119,27 @@ if __name__ == '__main__':
         else:
             sys.stdout.write('%s\t%d\t%d\t%.4f\n'%('\t'.join(out), ref+alt, alt, alt*1.0/(alt + ref)))
 
+    def outputGPGenotype(ss):
+        '''output genotype frequency based on imputation GP tag'''
+        out = ss[0:2] + ss[3:5]
+        values = []
+        for x in ss[vcfMetaCols:]:
+            values.append([float(t) for t in getGeno(x).split(',')])
+
+        count = np.sum(np.array(values), axis=0)
+        [out.append('%.3f'%(x)) for x in count]
+        sys.stdout.write('%s\n'%('\t'.join(out)))
+
     infile = VariantFile('-', 'r')
     #sys.stdout.write(str(infile.header))
-    sys.stdout.write('#CHROM\tPOS\tREF\tALT\tTotalCount\tAltCount\tAltFre\n')
     for line in infile:
         ss = str(line).strip().split()
         setoutGenoArrayIndex(ss[8])
 
         if OUT_FORMAT == 'ALT_FRE':
             outputAlleleFrequency(ss)
-
+        elif OUT_FORMAT == 'GP_GENO':
+            outputGPGenotype(ss)
 
     infile.close()
 sys.stdout.flush()
--- python/vcf/VCFAlleleCountsFre/bash.sh
+++ python/vcf/VCFAlleleCountsFre/bash.sh
@@ -1,2 +1,4 @@
 
 zcat test1.vcf.gz | python3 VCFAlleleCountsFre.py |bgzip >out.gz
+
+gzcat test.gp.vcf.gz | python3 VCFAlleleCountsFre.py -c GP_GENO
