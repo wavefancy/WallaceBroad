c2daf93567c3eb914a52cefa1cae284ca8e78d8e
Wallace Wang <wavefancy@gmail.com>
2019-05-02 09:38:11 -0400

    GT

--- python/vcf/VCFAlleleCountsFre/bash.sh
+++ python/vcf/VCFAlleleCountsFre/bash.sh
@@ -1,4 +1,5 @@
 
-zcat test1.vcf.gz | python3 VCFAlleleCountsFre.py |bgzip >out.gz
+# zcat test1.vcf.gz | python3 VCFAlleleCountsFre.py |bgzip >out.gz
+gzcat test1.vcf.gz | python3 VCFAlleleCountsFre.py -c GT_GENO | bgzip >GT.COUNTs.gz
 
-gzcat test.gp.vcf.gz | python3 VCFAlleleCountsFre.py -c GP_GENO
+#gzcat test.gp.vcf.gz | python3 VCFAlleleCountsFre.py -c GP_GENO
