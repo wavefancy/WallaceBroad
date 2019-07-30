ca379c665463108950169668518bf47ad9e94bf1
Wallace Wang <wavefancy@gmail.com>
2019-07-30 11:32:03 -0400

    add the option for reverse selection.

--- python/utils/wcut/wcut.py
+++ python/utils/wcut/wcut.py
@@ -6,13 +6,14 @@
     @Author: wavefancy@gmail.com
 
     Usage:
-        wcut.py (-f string | --tf file | -t titles) [-d string] [-c] [-a string] [--cs txt]
+        wcut.py (-f string | --tf file | -t titles) [-d string] [-c] [-a string] [--cs txt] [-r]
         wcut.py -h | --help | -v | --version | --format
 
     Options:
         -f string  Specifiy the columns want to cut.(eg. -f4,3; -f4,2-1, -f5-2)
         -t titles  Open title mode, one or more titles. eg. -t title1,title2,title3
         -d string  Set the delimiter for fields. 'tab' for Tab.'\\t'
+        -r         Do reverse selection.
         --tf file  Read title columns from file. (eg. -tf filename).
                         columns separated by WHITESPACE.
         -c         Open copy mode, directly copy comment line to stdout, comments started by '#'.
@@ -28,7 +29,7 @@
            is dependent on the occurance order in -f option, not the occurance in input file.
         2. (-t|-tf mode): If -t specified, the fist line were treated as title.
            output based on the occurance oder of input parameters.
-        3. Both for the input and output, columns are seperated by whitespace('\\t').
+        3. Both for the input and output, columns are seperated by whitespace('\\t') tab.
         4. Read input from stdin, and output to stdout.
         5. Column index starts from 1.
 """
@@ -60,6 +61,7 @@ class P(object):
     maxSplitTime = 0
     isSplitFixed = False #whether split a fix number of times.
     aVal = '' #whether add value to empty fields.
+    reverseSelection = False
 
 def runApp():
     # print(P.delimiter)
@@ -75,8 +77,15 @@ def runApp():
                 ii = ss.index(x)
                 P.outArrayId.append(ii)
             except ValueError:
-                sys.stderr.write('In title,can\'t find: %s\nPlease check!\n'%(x))
-                sys.exit(-1)
+                sys.stderr.write('WARN: In title,can\'t find: %s\n'%(x))
+                # sys.exit(-1)
+
+        # update P.outArrayId for reverse selection mode.
+        if P.reverseSelection:
+            pset = set(P.outArrayId)
+            rs = [x for x in range(len(ss)) if x not in pset]
+            P.outArrayId = rs
+
         #output title line
         s_out = [ss[i] for i in P.outArrayId]
         sys.stdout.write('\t'.join(s_out))
@@ -118,6 +127,12 @@ def runApp():
                         [t_out.append(k) for k in range(t_out[-1]+1, len(ss))]
                 P.outArrayId = t_out
                 P.First = False
+
+                # update P.outArrayId for reverse selection mode.
+                if P.reverseSelection:
+                    pset = set(P.outArrayId)
+                    rs = [x for x in range(len(ss)) if x not in pset]
+                    P.outArrayId = rs
 
             if not P.isSplitFixed:
                 P.maxSplitTime = max(P.outArrayId) +1
@@ -187,5 +202,8 @@ if __name__ == '__main__':
         P.title = True
         P.First = False # -t mode, do not need expand.
         P.tArray = args['-t'].split(',')
+    
+    if args['-r']:
+        P.reverseSelection = True
 
     runApp()
