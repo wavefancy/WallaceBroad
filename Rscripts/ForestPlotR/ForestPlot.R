9d67e319f33d80890589bae70158075e88abc0b0
Wallace Wang <wavefancy@gmail.com>
2020-01-14 15:39:03 -0500

    workable version 1.0

--- Rscripts/ForestPlotR/ForestPlot.R
+++ Rscripts/ForestPlotR/ForestPlot.R
@@ -8,7 +8,7 @@ Notes:
   * INPUT format are tsv file.
 
 Usage:
-  ForestPlot.R -o <filename> -W float -H float [-g name] [-x xlabel]
+  ForestPlot.R -o <filename> -W float -H float [-g name] [-x xlabel] [--xlim nums]
 
 Options:
   -o <filename> Output file name, in pdf format. eg. example.pdf
@@ -16,6 +16,7 @@ Options:
   -H float      The height of the output figure.
   -g name       Split the input by "name", plot each susbset by a section.
   -x xlabel     Set the X label name, default "Log(Odds Ratio)".
+  --xlim nums   Set the xlim, num1,num2
 
   ' -> doc
 
@@ -24,6 +25,7 @@ suppressMessages(library(Publish))
 suppressMessages(library(tidyverse))
 suppressMessages(library(docopt))
 suppressMessages(library(rio))
+suppressMessages(library(data.table))
 
 opts <- docopt(doc)
 # what are the options? Note that stripped versions of the parameters are added to the returned list
@@ -40,53 +42,85 @@ xname = 'Log(Odds Ratio)'
 if(is.null(opts$x) == F){
   xname = opts$x
 }
+xlim = c()
+if(is.null(opts$xlim) == F){
+  xlim = as.numeric(unlist(strsplit(opts$xlim,',')))
+}
 
 # check.names = F, to make the header can contain special characters.
 data = read.table(file("stdin"),header = T,sep="\t", check.names = FALSE)
-str(colnames(data))
 colnames(data) = str_replace_all(colnames(data),'X_n_','\n')
 colnames(data) = str_replace_all(colnames(data),'_n_','\n')
-str(colnames(data))
-str(data)
 
 rnames = c('BETA','BETAL','BETAR') #columns for required.
 # TEXTs to toshow
-texts = data %>% select(-!!rnames)
+texts = data %>% select(-!!rnames) %>% as.data.table()
 if( is.na(group) == F){
-  texts = texts %>% select(-!!c(group)) #remove the group colum for display as colum.
-  texts = split(texts,data[[group]])
+  # Import to use data.table split. more advance, can keep order.
+  texts = split(texts,by=group,keep.by = F)
 }
-
 #pdf(paste0(oname, '.pdf'),width=9,height=4.5,pointsize=16)
 pdf(ofile,width=W, height=H, pointsize=16)
-plotConfidence(
-                x = data$BETA,lower = data$BETAL, upper = data$BETAR
-                ,labels = texts
-                #,title.labels=tnames
-                ,xlab = xname
-                ,xaxis.cex=1.0    # font size for x axis.
-                ,xlab.cex=0.8     # font size for x label.
-                ,xlab.line=1.8    # The space between X label with X tick values.
-#               ,plot.log="x"
-                ,values=FALSE     # Hidden to show the values for beta and CI for beta. to want to show OR text instead.
-                ,order=c(1,2,3)
-                ,xratio=c(0.8,0.1)
 
-                ,refline = 0,refline.col='black',refline.lty=2
-                # ,xlim=c(-0.5,2.5)
-                ,y.title.offset=2.3 # the vertical space between title line with value.
-                ,section.sep=0.6    # the space between groups(splits).
-                ,title.line.col='black'
-                ,title.labels.cex = 1 # Set the font size for title line.
-                ,arrows.col='black'
-                ,points.col='black'
-                ,points.cex=1.5
-                ,arrows.lwd = 1.5
-#                 ,title.labels.pos=3
-#                 ,title.labels.y=10.5
-#                 ,title.labels.offset=0.5
-)
+if(is.null(opts$xlim) == F){
+  plotConfidence(
+                  x = data$BETA,lower = data$BETAL, upper = data$BETAR
+                  ,labels = texts
+                  #,title.labels=tnames
+                  ,xlab = xname
+                  ,xaxis.cex=1.0    # font size for x axis.
+                  ,xlab.cex=0.8     # font size for x label.
+                  ,xlab.line=1.8    # The space between X label with X tick values.
+  #               ,plot.log="x"
+                  ,values=FALSE     # Hidden to show the values for beta and CI for beta. to want to show OR text instead.
+                  ,order=c(1,2,3)
+                  ,xratio=c(0.8,0.1)
+
+                  ,refline = 0,refline.col='black',refline.lty=2
+                  ,xlim=xlim
+                  ,y.title.offset=2.3 # the vertical space between title line with value.
+                  ,section.sep=0.6    # the space between groups(splits).
+                  ,title.line.col='black'
+                  ,title.labels.cex = 1 # Set the font size for title line.
+                  ,arrows.col='black'
+                  ,points.col='black'
+                  ,points.cex=1.5
+                  ,arrows.lwd = 1.5
+  #                 ,title.labels.pos=3
+  #                 ,title.labels.y=10.5
+  #                 ,title.labels.offset=0.5
+  )
+}else{
+  plotConfidence(
+                  x = data$BETA,lower = data$BETAL, upper = data$BETAR
+                  ,labels = texts
+                  #,title.labels=tnames
+                  ,xlab = xname
+                  ,xaxis.cex=1.0    # font size for x axis.
+                  ,xlab.cex=0.8     # font size for x label.
+                  ,xlab.line=1.8    # The space between X label with X tick values.
+  #               ,plot.log="x"
+                  ,values=FALSE     # Hidden to show the values for beta and CI for beta. to want to show OR text instead.
+                  ,order=c(1,2,3)
+                  ,xratio=c(0.8,0.1)
+
+                  ,refline = 0,refline.col='black',refline.lty=2
+                  # ,xlim=NULL
+                  ,y.title.offset=2.3 # the vertical space between title line with value.
+                  ,section.sep=0.6    # the space between groups(splits).
+                  ,title.line.col='black'
+                  ,title.labels.cex = 1 # Set the font size for title line.
+                  ,arrows.col='black'
+                  ,points.col='black'
+                  ,points.cex=1.5
+                  ,arrows.lwd = 1.5
+  #                 ,title.labels.pos=3
+  #                 ,title.labels.y=10.5
+  #                 ,title.labels.offset=0.5
+  )
+}
+
 
 #mtext('Top percentile',side=3, at=c(-4),line=1.5)
 #text(-0.5,0,'TESTTESTTESTTEST')
-dev.off()
+t=dev.off()
--- Rscripts/ForestPlotR/bash.sh
+++ Rscripts/ForestPlotR/bash.sh
@@ -1,3 +1,5 @@
 # Test for line header has two lines.
 # Have to make every column to show has two lines.
-cat forest.middle-wgs.txt | Rscript ./ForestPlot.R -o test.pdf -W 9 -H 4.5 -g GROUP
+# cat forest.middle-wgs.txt | Rscript ./ForestPlot.R -o test.pdf -W 9 -H 4.5 -g GROUP
+cat forest.middle_UKB_V2_sw.txt | Rscript ./ForestPlot.R -o test_UKB_sw.pdf -W 9 -H 4.5 -g GROUP
+cat forest.middle_UKB_V2_ws.txt | Rscript ./ForestPlot.R -o test_UKB_ws.pdf -W 9 -H 4.5 -g GROUP --xlim 0,2.5
