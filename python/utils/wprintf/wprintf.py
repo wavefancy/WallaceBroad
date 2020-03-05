662b887ca1de22e497c8d00b1862e55bf63039b5
Wallace Wang <wavefancy@gmail.com>
2020-03-05 13:08:22 -0500

    add function for single line mode.

--- python/utils/wprintf/wprintf.py
+++ python/utils/wprintf/wprintf.py
@@ -6,14 +6,17 @@
     @Author: wavefancy@gmail.com
 
     Usage:
-        wprintf.py <pattern> [-l] [-s]
+        wprintf.py <pattern> [-c] [-s] [-l]
         wprintf.py -h | --help | -v | --version | -f | --format
 
     Notes:
 
     Options:
         -d pattern    The string pattern for format output.
-        -l            Collapse the output as a single line.
+        -c            Collapse the output as a single line.
+        -l            In line mode, do not split at each input line.
+                        In this mode, we can only have one place holder,
+                        as we only have one argument.
         -s            Set the place holder as [], instead of {}.
         -h --help     Show this screen.
         -v --version  Show version.
@@ -33,7 +36,7 @@ def ShowFormat():
 1 2 3
 K L M
 
-cat test.txt | python3 ./wprintf.py 'A{} B{} C{}' -l
+cat test.txt | python3 ./wprintf.py 'A{} B{} C{}' -c
 ------------------------
 A1 B2 C3 AK BL CM
 
@@ -42,12 +45,18 @@ cat test.txt | python3 ./wprintf.py 'A[] B[] C[]' -s
 ------------------------
 A1 B2 C3
 AK BL CM
+
+cat test.txt | python3 ./wprintf.py 'linemode {}' -l
+------------------------
+linemode 1 2 3
+linemode K L M
 ''')
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='3.0')
 
-    singleline = True if args['-l'] else False
+    singleline = True if args['-c'] else False
+    linemode   = True if args['-l'] else False
 
     if(args['--format']):
         ShowFormat()
@@ -61,10 +70,15 @@ if __name__ == '__main__':
     for line in sys.stdin:
         line = line.strip()
         if line:
-            ss = line.split()
+            if linemode:
+                out = stingpattern.format(line)
+            else:
+                ss = line.split()
+                out = stingpattern.format(*ss)
+
             # api: https://www.geeksforgeeks.org/python-format-function/
             # *ss for fitting a array list.
-            out = stingpattern.format(*ss)
+            
             if singleline:
                 out_array.append(out)
             else:
