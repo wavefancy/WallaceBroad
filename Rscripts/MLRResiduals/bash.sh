1ee1c9c4b0f9c96e5bb8665ae2771bf709e37b28
Wallace Wang <wavefancy@gmail.com>
2020-05-31 06:58:55 -0400

    output include all the columns from input

--- Rscripts/MLRResiduals/bash.sh
+++ Rscripts/MLRResiduals/bash.sh
@@ -1,8 +1,9 @@
 
-cat test.txt | Rscript MLRResiduals.R
+cat test.txt | Rscript MLRResiduals.R -f 'prestige~census+type'
 
-#2.66730545059386
-#6.30558060408625
-#-3.13374677327293
-#-6.30558060408625
-#0.46644132267907
+#prestige census type RESIDUALS
+#68.8 1113 prof 2.66730545059386
+#69.1 1130 great 6.30558060408625
+#63.4 1171 prof -3.13374677327293
+#56.8 1175 great -6.30558060408625
+#73.5 2111 prof 0.466441322679069
