df3eca8543c3152a431fa75b6638d8e3ba4e67a8
Wallace Wang <wavefancy@gmail.com>
2020-07-05 01:00:45 -0400

    version 1.0

---
+++ Rscripts/IQROutliers/IQROutliers.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+# @Wallace Wang, wavefancy@gmail.com
+#------------------------------
+
+# IQR rule and implementation details:
+# 1. https://www.khanacademy.org/math/statistics-probability/summarizing-quantitative-data/box-whisker-plots/a/identifying-outliers-iqr-rule
+# 2. https://stackoverflow.com/questions/12866189/calculating-the-outliers-in-r
+"
+=======================================================================================
+Detect outliers based on 1.5xIQR rule.
+
+    A commonly used rule says that a data point is an outlier 
+    if it is more than 1.5⋅IQR above the third quartile 
+    or below the first quartile. Said differently, low outliers 
+    are below Q1−1.5⋅IQR and high outliers are above Q3+1.5⋅IQR.
+
+Usage:
+    IQROutliers.R -n name
+    IQROutliers.R -h [--help]
+
+Options:
+   -n name   Column name for checking outliers.
+
+Notes:
+    1. Read tsv data from stdin and output results to stdout.
+    2. Input should without missing data for column '-n.'
+=======================================================================================
+" -> doc
+options(warn=-1)
+suppressMessages(library(docopt))
+
+opts <- docopt(doc)
+
+# define a function to remove outliers
+FindOutliers <- function(data) {
+  lowerq = quantile(data)[2]
+  upperq = quantile(data)[4]
+  iqr = upperq - lowerq #Or use IQR(data)
+  # we identify extreme outliers
+  extreme.threshold.upper = (iqr * 1.5) + upperq
+  extreme.threshold.lower = lowerq - (iqr * 1.5)
+  result <- which(data > extreme.threshold.upper | data < extreme.threshold.lower)
+}
+
+#dd = read.table("test.txt",header = T)
+dd = read.table(file("stdin"),header = T,check.names=F)
+out = FindOutliers(dd[[opts$n]])
+
+write.table(dd[out,] ,"",row.names=F,col.names=T,quote=F)
---
+++ Rscripts/IQROutliers/bash.sh
@@ -0,0 +1,3 @@
+cat test.txt | Rscript IQROutliers.R -n T2
+T1 T2
+E 20
