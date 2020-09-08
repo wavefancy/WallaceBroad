7a7962d2abedbb4a7634c23c18a738af4150cba2
Wallace Wang <wavefancy@gmail.com>
2020-09-08 07:29:47 -0400

    add delimiter option.

--- python/utils/AddColumn/AddColumn.py
+++ python/utils/AddColumn/AddColumn.py
@@ -7,7 +7,7 @@
     @Author: wavefancy@gmail.com, Wallace Wang.
 
     Usage:
-        AddColumn.py (-c int|-l) (-v text | -i int | -e ints) [-t txt]
+        AddColumn.py (-c int|-l) (-v text | -i int | -e ints) [-t txt] [-d text]
         AddColumn.py -h | --help | --version | -f | --format
 
     Notes:
@@ -17,14 +17,15 @@
         -c int        After which column(int, 1 based) from the input for insertting new column.
                         0 for add column at the line begining.
         -l            Add extra column to the last of a line.
-        -v text       The value for the new insertting column(s).
+        -v text       The value for the new inserting column(s).
                         eg. v1|v1,v2. v1,v2 add two columns, split values by ','
-        -t text       The title for the new insertting column(s).
+        -t text       The title for the new inserting column(s).
                         eg. t1|t1,t2. Split values by ','. len(-t) == len(-v).
         -e ints       Using the Existing columns as the content for the new column.
                         eg. 1|1,3. ints for the column index, based on input file, 1 based.
                         Copied columns were concatenated by ':'.
         -i int        Add index value for each row, index start from 'int'.
+        -d text       Set up the field delimiter, default tab.
         -h --help     Show this screen.
         --version     Show version.
         -f --format   Show input/output file format example.
@@ -72,6 +73,7 @@ if __name__ == '__main__':
     INDEXV  = int(args['-i'])       if args['-i'] else None # add index value.
     TITLES  = args['-t'].split(',') if args['-t'] else VALS
     COPY_VALS = [int(x)-1 for x in args['-e'].split(',')] if args['-e'] else None
+    DELIMITER = args['-d'] if args['-d'] else '\t'
     # print(COPY_VALS)
 
     if VALS and len(VALS) != len(TITLES):
@@ -81,7 +83,7 @@ if __name__ == '__main__':
     for line in sys.stdin:
         line = line.strip()
         if line :
-            ss = line.split()
+            ss = line.split(DELIMITER)
             if LAST:
                 COL = len(ss)
                 LAST = False
@@ -106,7 +108,7 @@ if __name__ == '__main__':
                     sys.stderr.write('ERROR: please set proper parameters!\n')
                     sys.exit(-1)
 
-            sys.stdout.write('%s\n'%('\t'.join(out)))
+            sys.stdout.write('%s\n'%(DELIMITER.join(out)))
 
 sys.stdout.flush()
 sys.stdout.close()
