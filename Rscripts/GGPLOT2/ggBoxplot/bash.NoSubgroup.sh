d6ed7a98fae420caa2721bda4441fc453da9f1d1
Wallace Wang <wavefancy@gmail.com>
2020-10-27 14:22:21 -0400

    add option for nobox, add dots to box plot.

---
+++ Rscripts/GGPLOT2/ggBoxplot/bash.NoSubgroup.sh
@@ -0,0 +1,11 @@
+
+wecho "
+    gunzip -dc toothgrowth.csv.gz
+        | /Users/minxian/Broad/Program/miniconda3/envs/forestplot/bin/Rscript ./ggBoxplot.R
+        -x dose -y len
+        #-c supp
+        --cp 'red::black::#8577FF00'
+        -o test.nosub.pdf -W  4 -H 3
+        --xo '2::0.5::1' --wl 0
+        --nb
+"
