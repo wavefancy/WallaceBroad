17f8cb61d92aaae61d25561970851760ca217202
Wallace Wang <wavefancy@gmail.com>
2020-12-09 17:16:33 -0500

    count line and append log.

--- python/utils/wcat/bash.sh
+++ python/utils/wcat/bash.sh
@@ -1,6 +1,8 @@
-python3 ./wcat.py test1.txt.gz test2.txt test3.txt.zst
-
-ls test* | python3 ./wcat.py
+wecho "
+      python3 ./wcat.py test1.txt.gz test2.txt test3.txt.zst
+    !!python3 ./wcat.py -c log.txt -a -m 'MYLOG' test1.txt.gz test2.txt test3.txt.zst
+    !!ls test* | python3 ./wcat.py -i -c log.txt -a -m 'STDIN'
+"
 
 #A
 #B
--- python/utils/wcat/wcat.py
+++ python/utils/wcat/wcat.py
@@ -3,18 +3,26 @@
 """
 
     An enhanced `cat` like program, with support read gz, zst compressed files.
-    - Read file list from stdin or from command line arguments.
+    *** 1) Support read file list from stdin or from command line arguments.
+    *** 2) Support read content from stdin, and output to stdout.
+    *** 3) Support count the number of lines program processed and log it to file.
+    
     @Author: wavefancy@gmail.com
 
     Usage:
-        wcat.py [<files>...]
+        wcat.py [-i] [-c file] [-a] [-m txt] [<files>...]
         wcat.py -h | --help | -v | --version | -f | --format
 
     Notes:
         1. Concatenate multiple files together, order as the appearence in parameters.
-        3. Input files either in ziped(.gz), zst(.zst), or flat txts.
+        2. Input files either in ziped(.gz), zst(.zst), or flat txts.
 
     Options:
+        -i            Treat the stdin as content input, other than file names.
+        -c file       Count the total number of lines program processed, 
+                        and log the message to file.
+        -m txt        Set the log message for parameter '-c'.
+        -a            Append the log message to file ['-c'], instead of a new file.
         -h --help     Show this screen.
         -v --version  Show version.
         -f --format   Show input/output file format example.
@@ -43,42 +51,61 @@ if __name__ == '__main__':
         ShowFormat()
         sys.exit(-1)
 
-    # read file names from stdin or from parameters.
-    names = args['<files>'] if args['<files>'] else []
-    if not names:
+    LOG_FILE = args['-c'] if args['-c'] else False
+    ReadFileNames = False if args['-i'] else True
+    TotalLines = 0
+
+    if ReadFileNames:
+        # read file names from stdin or from parameters.
+        names = args['<files>'] if args['<files>'] else []
+        if not names:
+            for line in sys.stdin:
+                line = line.strip()
+                if line:
+                    names.append(line)
+
+        import gzip
+        import zstandard as zstd
+        files = []
+        zstfiles = []
+        for f in names:
+            if  f.endswith('.gz'):
+                files.append(gzip.open(f, 'rt'))
+            elif f.endswith('.zst'):
+                # https://pypi.org/project/zstandard/, use text_stream to support read line by line.
+                zst_temp = open(f, 'rb')
+                dctx = zstd.ZstdDecompressor()
+                stream_reader = dctx.stream_reader(zst_temp)
+                text_stream = io.TextIOWrapper(stream_reader, encoding='utf-8')
+                # cached files for read and close.
+                files.append(text_stream)
+                zstfiles.append(zst_temp)
+            else:
+                #files.append(open(f,'r'))
+                files.append(open(f,'r',encoding="utf-8"))
+
+        # sys.stdout.reconfigure(encoding='utf-8')
+        for file in files:
+            for line in file:
+                TotalLines += 1
+                sys.stdout.write(line)
+
+        #close files.
+        [f.close() for f in files]
+        [f.close() for f in zstfiles]
+    else:
+        # read content from stdin.
         for line in sys.stdin:
-            line = line.strip()
-            if line:
-                names.append(line)
-
-    import gzip
-    import zstandard as zstd
-    files = []
-    zstfiles = []
-    for f in names:
-        if  f.endswith('.gz'):
-            files.append(gzip.open(f, 'rt'))
-        elif f.endswith('.zst'):
-            # https://pypi.org/project/zstandard/, use text_stream to support read line by line.
-            zst_temp = open(f, 'rb')
-            dctx = zstd.ZstdDecompressor()
-            stream_reader = dctx.stream_reader(zst_temp)
-            text_stream = io.TextIOWrapper(stream_reader, encoding='utf-8')
-            # cached files for read and close.
-            files.append(text_stream)
-            zstfiles.append(zst_temp)
-        else:
-            #files.append(open(f,'r'))
-            files.append(open(f,'r',encoding="utf-8"))
-
-    # sys.stdout.reconfigure(encoding='utf-8')
-    for file in files:
-        for line in file:
+            TotalLines += 1
             sys.stdout.write(line)
 
-    #close files.
-    [f.close() for f in files]
-    [f.close() for f in zstfiles]
+    if LOG_FILE:
+        mode = 'a' if args['-a'] else 'w'
+        with open(LOG_FILE, mode=mode) as a_writer:
+            if args['-m']:
+                a_writer.write('%s. Total number of line: %d\n'%(args['-m'],TotalLines))
+            else:
+                a_writer.write('Total number of line: %d\n'%(TotalLines))
 
 sys.stdout.flush()
 sys.stdout.close()
