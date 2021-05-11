06b2fc13478ed70ed08fc8605c42448d9a1ea7b7
Wallace Wang <wavefancy@gmail.com>
2021-05-11 04:11:39 -0400

    refine the document.

--- python/Annotation/ResetDbNSFPDamaging/ResetDbNSFPDamaging.py
+++ python/Annotation/ResetDbNSFPDamaging/ResetDbNSFPDamaging.py
@@ -42,17 +42,16 @@ def ShowFormat():
 # dbNSFP 3.5a ReadMe, variant classification
 # https://drive.google.com/file/d/0B60wROKy6OqcNGJ2STJlMTJONk0/view
 
-# .       40391751        0.268060
-#D       63232977        0.419646
-#T       47056885        0.312293
+
+# Below summary statistics based on: dbNSFP3.5a.txt.gz
+# /medpop/esp/wallace/VEP/data
 # SIFT_pred: If SIFTori is smaller than 0.05 (rankscore>0.395) the corresponding nsSNV is
 #		predicted as "D(amaging)"; otherwise it is predicted as "T(olerated)".
 #		Multiple predictions separated by ";"
+# .       40391751        0.268060
+#D       63232977        0.419646
+#T       47056885        0.312293
 
-#.       11027847        0.102315
-#B       34489731        0.319991
-#D       39291666        0.364542
-#P       22974338        0.213152
 # Polyphen2_HDIV_pred: Polyphen2 prediction based on HumDiv, "D" ("probably damaging",
 #		HDIV score in [0.957,1] or rankscore in [0.52844,0.89865]), "P" ("possibly damaging",
 #		HDIV score in [0.453,0.956] or rankscore in [0.34282,0.52689]) and "B" ("benign",
@@ -61,11 +60,12 @@ def ShowFormat():
 #		"neutral" if the HDIV score is smaller than 0.5 (rankscore is smaller than 0.3528),
 #		and "deleterious" if the HDIV score is larger than 0.5 (rankscore is larger than
 #		0.3528). Multiple entries are separated by ";".
+#.       11027847        0.102315
+#B       34489731        0.319991
+#D       39291666        0.364542
+#P       22974338        0.213152
+# D + P = 0.577694
 
-#.       11027847        0.102084
-#B       42428265        0.392754
-#D       31698431        0.293429
-#P       22873087        0.211734
 # Polyphen2_HVAR_pred: Polyphen2 prediction based on HumVar, "D" ("probably damaging",
 #		HVAR score in [0.909,1] or rankscore in [0.62797,0.97092]), "P" ("possibly damaging",
 #		HVAR in [0.447,0.908] or rankscore in [0.44195,0.62727]) and "B" ("benign", HVAR
@@ -74,50 +74,57 @@ def ShowFormat():
 #		is "neutral" if the HVAR score is smaller than 0.5 (rankscore is smaller than
 #		0.45833), and "deleterious" if the HVAR score is larger than 0.5 (rankscore is larger
 #		than 0.45833). Multiple entries are separated by ";".
+#.       11027847        0.102084
+#B       42428265        0.392754
+#D       31698431        0.293429
+#P       22873087        0.211734
+# D + P = 0.505163
 
-#.       16107458        0.193407
-#D       36818886        0.442094
-#N       25582396        0.307175
-#U       4774119 0.057324
 # LRT_pred: LRT prediction, D(eleterious), N(eutral) or U(nknown), which is not solely
 #		determined by the score.
+#.       16107458        0.193407
+#N       25582396        0.307175
+#U       4774119         0.057324
+#D       36818886        0.442094
 
-# .       2246141 0.009169
-#A       8707555 0.035546
-#D       156000864       0.636828
-#N       77834460        0.317736
-#P       176452  0.000720
+
 # MutationTaster_pred: MutationTaster prediction, "A" ("disease_causing_automatic"),
 #		"D" ("disease_causing"), "N" ("polymorphism") or "P" ("polymorphism_automatic"). The
 #		score cutoff between "D" and "N" is 0.5 for MTnew and 0.31713 for the rankscore.
+# .      2246141         0.009169
+#A       8707555         0.035546
+#D       156000864       0.636828
+#N       77834460        0.317736
+#P       176452          0.000720
+# A + D = 0.672374
 
 # MetaSVM_pred
-#.       7519568 0.083907
-#D       13663364        0.152463
+#.       7519568         0.083907
 #T       68434853        0.763630
+#D       13663364        0.152463
 
 #M-CAP_pred
 #.       11583101        0.129250
-#D       40128069        0.447769
 #T       37906615        0.422981
+#D       40128069        0.447769
 
 #MetaLR_pred
-#.       7519568 0.083907
-#D       14557286        0.162437
+#.       7519568         0.083907
 #T       67540931        0.753655
+#D       14557286        0.162437
 
-#MutationAssessor_pred
-#.       20158730        0.224941
-#H       4895251 0.054624
-#L       22645922        0.252695
-#M       28229553        0.314999
-#N       13688329        0.152741
 # MutationAssessor_pred: MutationAssessor's functional impact of a variant :
 #		predicted functional, i.e. high ("H") or medium ("M"), or predicted non-functional,
 #		i.e. low ("L") or neutral ("N"). The MAori score cutoffs between "H" and "M",
 #		"M" and "L", and "L" and "N", are 3.5, 1.935 and 0.8, respectively. The rankscore cutoffs
 #		between "H" and "M", "M" and "L", and "L" and "N", are 0.92922, 0.51944 and 0.19719,
 #		respectively.
+#MutationAssessor_pred
+#.       20158730        0.224941
+#H       4895251         0.054624
+#L       22645922        0.252695
+#M       28229553        0.314999
+#N       13688329        0.152741
 
 # Mark's damaging mask.
 #Scores used for 'of five' designations:
