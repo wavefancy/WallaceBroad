d5e797ab2d5327a337574d3b3c24a74b0c373b74
minxian wang <minxianwang@minxians-MacBook-Pro.local>
2021-07-29 05:52:57 -0400

    title and missing as NA

--- python/utils/RowScoreMixer/RowScoreMixer.py
+++ python/utils/RowScoreMixer/RowScoreMixer.py
@@ -12,8 +12,13 @@
 
     Notes:
         1. Read scores from stdin, and output results to stdout.
-        2. Input format:  the first col is name, used to associate weight, from 2-n are scores. 
-        3. Output format: 1st colum: NumberOfGenes:GeneNames:Weights, 2-n are the combined scores. 
+        2. Input format: 
+            * 1st line is tittle. 
+            * the 1st col is name, used to associate weight, from 2-n are scores. 
+        3. Output format: 
+            * 1st colum: NumberOfGenesHasScore:GeneNames:Weights, 2-n are the combined scores.
+            * Genes don't have score associated with it, the output weight will be 'NA'.
+            * If less than one gene in the network has scores, the mixed scores were all 'NA'.
         4. See example by -f.
 
     Options:
@@ -34,6 +39,7 @@ def ShowFormat():
     print('''
 # INPUT:
 ------------------------
+Gene    NA1 NA2
 N1      1 2
 N2      2 3
 N3      1 5
@@ -42,12 +48,15 @@ N3      1 5
 ------------------------
 N1:1 N2:3
 N2:2 N3:4
+N2:2 N4:4
 
 #OUTPUT:
 cat test.txt | python3 ./RowScoreMixer.py -w weight.txt 
 ------------------------
+NumberOfGenesHasScore:GeneNames:Weights NA1 NA2
 2:N1,N2:1,3     7       11
 2:N2,N3:2,4     8       26
+1:N2,N4:2,NA    NA      NA
     ''');
 
 if __name__ == '__main__':
@@ -60,15 +69,21 @@ if __name__ == '__main__':
 
     # loading the scores.
     score_map = {}
+    Fill_NAs = []
     for line in sys.stdin:
         line = line.strip()
         if line:
             ss = line.split(None,1)
-            if ss[0] in score_map:
-                sys.stderr.write('ERROR: duplicated gene name in score file: %s\n'%(ss[0]))
-                sys.exit(-1)
+            if Fill_NAs:
+                if ss[0] in score_map:
+                    sys.stderr.write('ERROR: duplicated gene name in score file: %s\n'%(ss[0]))
+                    sys.exit(-1)
+                else:
+                    score_map[ss[0]] = ss[1]
             else:
-                score_map[ss[0]] = ss[1]
+                ss = line.split(None,1)
+                sys.stdout.write('NumberOfGenesHasScore:GeneNames:Weights\t%s\n'%(ss[1]))
+                Fill_NAs = '\t'.join(['NA' for x in ss[1].split()])
   
     import numpy as np
     with open(args['-w'], 'r') as nfile:
@@ -79,24 +94,32 @@ if __name__ == '__main__':
 
                 name = []
                 weight = []
+                out_name = []
+                out_weight = []
                 for x in ss:
                     xx = x.split(':')
+
+                    out_name.append(xx[0])
                     if xx[0] in score_map: # only put in genes we have score for it.
                         name.append(xx[0])
                         weight.append(xx[1])
+                        out_weight.append(xx[1])
+                    else:
+                        out_weight.append('NA')
 
                 if len(name) <= 1:
-                    continue
-                
-                # compute the weighted sum.
-                t_sum = np.array([float(x) for x in score_map[name[0]].split()]) * float(weight[0])
-                for g,w in zip(name[1:], weight[1:]):
-                    tt = np.array([float(x) for x  in score_map[g].split()]) * float(w)
-                    t_sum = t_sum + tt
+                    s = Fill_NAs
+                else:
+                    # compute the weighted sum.
+                    t_sum = np.array([float(x) for x in score_map[name[0]].split()]) * float(weight[0])
+                    for g,w in zip(name[1:], weight[1:]):
+                        tt = np.array([float(x) for x  in score_map[g].split()]) * float(w)
+                        t_sum = t_sum + tt
+                    
+                    s = '\t'.join(['%g'%(x) for x in t_sum])
 
-                n = ','.join(name)
-                ww = ','.join(weight)
-                s = '\t'.join(['%g'%(x) for x in t_sum])
+                n = ','.join(out_name)
+                ww = ','.join(out_weight)
                 sys.stdout.write('%d:%s:%s\t%s\n'%(len(name),n,ww,s))
 
 sys.stdout.flush()
--- python/utils/RowScoreMixer/bash.sh
+++ python/utils/RowScoreMixer/bash.sh
@@ -1,4 +1,6 @@
 cat test.txt | python3 ./RowScoreMixer.py -w weight.txt 
 
+#NumberOfGenesHasScore:GeneNames:Weights NA1 NA2
 #2:N1,N2:1,3     7       11
 #2:N2,N3:2,4     8       26
+#1:N2,N4:2,NA    NA      NA
